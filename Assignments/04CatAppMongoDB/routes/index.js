3b020d7fbc93f7db9cfd287cba12c8b63ae27e66
Sarah Walters <sarah.walters@students.olin.edu>
2016-02-16 13:09:47 -0500

    HW4 feedback

--- Assignments/04CatAppMongoDB/routes/index.js
+++ Assignments/04CatAppMongoDB/routes/index.js
@@ -1,11 +1,12 @@
 var mongoose = require('mongoose');
-var catModel = require('./../models/catModel.js');
+var express = require('express');
+var catModel = require('./../models/catModel.js'); // Conventionally you'd name this variable "Cat" -- constructors are capitalized
+var db = require('../fakeDatabase');
+// It's usually good practice to group your dependencies based on where they're
+// coming from -- all external dependencies together (mongoose and express),
+// all internal dependencies together (catModel and db), etc.
 
 var router = {};
-
-var express = require('express');
-
-var db = require('../fakeDatabase');
 
 var nameList = ["Tom", "James", "Shruti", "Sherry"];
 
@@ -13,13 +14,15 @@ var nameList = ["Tom", "James", "Shruti", "Sherry"];
 function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
 }
+
 function getColor() {
   colorList = ['black', 'white', 'yellow', 'gray', 'brown'];
   firstColor = colorList[getRandomInt(0, colorList.length-1)];
   secondColor = colorList[getRandomInt(0, colorList.length-1)];
   return [firstColor, secondColor];
 }
-//function that constructs and returns lizard object
+
+//function that constructs and returns cat object
 function getCat(){
   var cat = {
 	name: nameList[getRandomInt(0,nameList.length-1)],
@@ -58,6 +61,8 @@ function findOldestCat(cats){
 }
 
 function sortCatsbyColor(paramColor, cats){
+	// This is a great place to use Array.filter -- does exactly what you're doing here
+	// and requires MUCH less work from you. Maybe more efficient, too!
 	var colorCats = [];
 	for (var i = 0; i < cats.length; i++){
 		console.log("color[0]" + cats[i].color[0]);
@@ -71,68 +76,57 @@ function sortCatsbyColor(paramColor, cats){
 }
 
 
-//get all lizard names
+//get all cat names
 router.getAllCats = function(req, res, next){
-	// var cats = db.getAll();
-
 	catModel.find(function(err, data) {
 		var cats = data;
 		cats = sortCats(cats);
 		console.log(cats);
 		var message = "All the cats are: "
-		res.render("allCats", {message: message, cats: cats});	
+		res.render("allCats", {message: message, cats: cats});
 	});
-}
+};
 
 
-// create new lizard named Bob
-
+// create new cat
 router.getNewCat = function(req, res, next) {
 	var cat = getCat();
-	// db.add(cat);
-	var message = "new cat: "
-	console.log(cat)
-	cats = [cat];
+	// var cats = [cat]; -- you'd want to use "var cats" so the variable is scoped within the function
+	// ... or you could just define cats as [cat] in the object, like I did below
 
 	var o = new catModel(cat);
 	o.save(function(err) {
-	res.render("allCats", {message: message, cats: cats});
+		res.render("allCats", {message: "new cat: ", cats: [cat]});
 	});
-}
+};
 
 
 
 router.getCatColor = function(req, res){
 	var color = req.params.color;
-	// var color = color.slice(1, color.length);
+	// var color = color.slice(1, color.length); // good, you don't need this. Go ahead and remove the comment next time!
 	catModel.find({}, function(err, data) {
-		// separate in stock & out of stock
 		var cats = data
 		cats = sortCatsbyColor(color, cats);
-		var message = "Find cats: ";
-		res.render("allCats", {message: message, cats: cats});
-		
-		});
-
-}
+		// You're finding all cats of a particular color by finding all cats and then
+		// running what's essentially a filter function over the list -- would be better
+		// to use a Mongoose query! That's what Mongoose is for.
+		res.render("allCats", {message: "Find cats: ", cats: cats});
+	});
+};
 
 router.deleteCat = function(req, res){
-	// var cats = db.getAll();
-	catModel.find({}, function(err, data) {
-		var cats = data
+	catModel.find({}, function(err, cats) {
+		// I went through and cleaned up extraneous variables -- this is a little cleaner
 		var cat = findOldestCat(cats);
-		catId = cat._id;
-		console.log(cat)
-		catModel.findOneAndRemove({"_id": catId}, function(err, data) {
-		var message = "After deleting: ";
-		catModel.find({}, function(err, data) {
-			var cats = data
-			res.render("allCats", {message: message, cats: cats});
-		});
-
+		catModel.findOneAndRemove({"_id": cat._id}, function(err, data) {
+			catModel.find({}, function(err, cats) {
+				res.render("allCats", {message: "After deleting: ", cats: cats});
+			});
+			// particularly when nesting callbacks, indentation is really important.
 		});
 	});
-}
+};
 
 router.home = function(req, res){
 	res.render("home");
