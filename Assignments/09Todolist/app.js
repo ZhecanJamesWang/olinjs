bdfa23b6c24d713177cde99bf5a63169578933f6
tangtangwzc <tangtangwzc@gmail.com>
2016-02-25 16:12:44 +0500

    updating

--- Assignments/09Todolist/app.js
+++ Assignments/09Todolist/app.js
@@ -1,32 +1,29 @@
-var exphbs = require('express-handlebars');
+// set up ========================
+var port = process.env.PORT || 8080;                // set the port
 var path = require('path');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var express = require('express');
-var index = require('./routes/index');
 var app = express();
-var routes = require('./routes/index');
 var mongoose = require('mongoose');
+var morgan = require('morgan');             // log requests to the console (express4)
+var methodOverride = require('method-override'); // simulate DELETE and PUT (express4)
+
+    // configuration =================
 
 /* CONNECT TO MONGOOSE */
 mongoose.connect(process.env.MONGOURI || 'mongodb://localhost/test');
+app.use(express.static(__dirname + '/public'));                 // set the static files location /public/img will be /img for users
+app.use(morgan('dev'));                                         // log every request to the console
+app.use(bodyParser.urlencoded({'extended':'true'}));            // parse application/x-www-form-urlencoded
+app.use(bodyParser.json());                                     // parse application/json
+app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
+app.use(methodOverride('X-HTTP-Method-Override')); // override with the X-HTTP-Method-Override header in the request
 
+// routes ======================================================================
+require('./routes/index.js')(app);
 
-app.engine('handlebars', exphbs({defaultLayout: 'main'}));
-app.set('view engine', 'handlebars');
-app.set('views', __dirname + '/views');
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, './public')));
-
-
-app.get('/', routes.home);
-app.get('/cats/new', routes.getNewCat);
-app.get('/cats', routes.getAllCats);
-app.get('/cats/bycolor/:color', routes.getCatColor);
-app.get('/cats/delete/old', routes.deleteCat);
-
-app.listen(3000);
+// listen (start app with node server.js) ======================================
+app.listen(port);
+console.log("App listening on port " + port);
--- Assignments/09Todolist/models/catModel.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose');
-
-var catSchema = mongoose.Schema({
-	name: String,
-	age: Number,
-	color: Array
-});
-
-module.exports = mongoose.model('catSchema', catSchema);
